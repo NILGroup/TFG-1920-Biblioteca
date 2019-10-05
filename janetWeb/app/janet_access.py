277265bc2c565576d79321ed2963083f98988627
root <miguel.angel.vk@hotmail.com>
2019-10-05 15:06:52 +0200

    La app web llama bien y escribe la respuesta en un log. Llevo 2 días para 5 lineas.

--- janetWeb/app/janet_access.py
+++ janetWeb/app/janet_access.py
@@ -2,13 +2,21 @@
 #Defines the ways to access or edit the information in the database
 import http.client
 from app import janet_host, janet_port
+import json
 
 #TODO darle formato al mensaje para que sea aceptado por janet, usarl JSONObjet
 #si es posible (fijarse en el codigo java de la app del movil)
 def sendMessage(m):
-    client = http.client.HTTPConnection(janet_host, janet_port)
-    client.connect()
-    client.request("POST","/api", m)
-    response = client.getresponse()
-    client.close()
-    return response
+	f=open("log.txt", "a+") #
+	client = http.client.HTTPConnection(janet_host, janet_port)
+	client.connect()
+	headers = {'Content-type': 'application/json'}
+	query = '&content=' + m + '&user_id=0&type=query&' #Si os preguntais por que & al principio y al final pues yo que se, pero asi funciona y si no el servidor recibe campos en blancoy pone " Detras de otros
+	json_data = json.dumps(query)
+	client.request("POST","/api", json_data, headers)
+	response = client.getresponse()
+	f.write("data:" + json_data + '\n')
+	f.write("response: " + response.read().decode('utf-8') + '\n')
+	f.close()
+	client.close()
+	return response
