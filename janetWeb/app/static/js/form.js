eb3bd22dc2462c3660fc16801e2238d247ad603a
marito07 <marito07@ucm.es>
2019-11-03 13:54:19 +0100

    Cambios visuales JanetWeb. Mejoras Bootstrap4.

--- janetWeb/app/static/js/form.js
+++ janetWeb/app/static/js/form.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 				message : $('#message').val()
 			},
 			type : 'POST',
-			url : '/process' 
+			url : '/process'
 		})
 		.done(function(data){
 			oldData = data;
@@ -21,22 +21,22 @@ $(document).ready(function() {
 				msg.lang = 'es';
 				window.speechSynthesis.speak(msg);
 
-				$('#messages').append("<div class='message inMessage'><p>" + data.response + "</p></div>");
+				$('#messages').append("<div class='row'><div class='col-9 col-md-5 message inMessage'><p>" + data.response + "</p></div></div>");
 
 				switch(data["content-type"]){
 					case "list-books":
 						$('#messages').append();
 						data.books.forEach(function(element) {
-							var htmlToApend = "<div class='message inMessage'>";
-							htmlToApend += getBookHTML(element.title, element.author); 
-							$('#messages').append(htmlToApend + "</div>");
+							var htmlToApend = "<div class='row'><div class='col-9 col-md-5 message inMessage'>";
+							htmlToApend += getBookHTML(element.title, element.author);
+							$('#messages').append(htmlToApend + "</div></div>");
 						});
 						break;
 
 					case "single-book":
-						var htmlToApend = "<div class='message inMessage'>";
-						htmlToApend += getBookHTML(data.title, data.author, data.url); 
-						$('#messages').append(htmlToApend + "</div>");
+						var htmlToApend = "<div class='row'><div class='col-9 col-md-5 message inMessage'>";
+						htmlToApend += getBookHTML(data.title, data.author, data.url);
+						$('#messages').append(htmlToApend + "</div></div>");
 						break;
 
 					case "location":
@@ -44,7 +44,7 @@ $(document).ready(function() {
 						break;
 
 					case "phone":
-						$('#messages').append("<div class='message inMessage'><h4>" + data.library + ": " + data.phone + "</h4></div>");
+						$('#messages').append("<div class='row'><div class='col-9 col-md-5 message inMessage'><h4>" + data.library + ": " + data.phone + "</h4></div></div>");
 						break;
 
 					default:
@@ -53,13 +53,13 @@ $(document).ready(function() {
 				newMessageScroll();
 			}
 			else {
-				$('#messages').append("<div class='message inMessage'><p>" + "No he podido realizar la consulta." + "</p></div>");
+				$('#messages').append("<div class='row'><div class='col-9 col-md-5 message inMessage'><p>" + "No he podido realizar la consulta." + "</p></div></div>");
 			}
 			$('#submit').attr("disabled", false)
 
 
 		});
-		$('#messages').append("<div class='message outMessage'><p>" + $('#message').val() + "<p></div>")
+		$('#messages').append("<div class='row justify-content-end'><div class='col-9 col-md-5 message outMessage'><p>" + $('#message').val() + "<p></div></div>")
 		newMessageScroll();
 		$('#submit').attr("disabled", true)
 		$('#message').val("")
@@ -72,11 +72,11 @@ $(document).ready(function() {
 function addMap(lat, long, location){
 	var mapid = 'map' + mapCount;
 	mapCount++; //Los mapas necesitan diferentes ids cada uno
-	$('#messages').append("<div class='message inMessage'><h4>" + location + "</h4>" + 
-			"<div class='map' id='" + mapid + "'></div></div>");
+	$('#messages').append("<div class='row'><div class='message inMessage'><h4>" + location + "</h4>" +
+			"<div class='map' id='" + mapid + "'></div></div></div>");
 
 	var mymap = L.map(mapid).setView([lat, long], 13); //Crea el mapa con un zoom de 13
-	
+
 	//Esto utiliza mapbox para cargar los tiles del mapa y que se vea
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
